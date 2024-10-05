f5f8906b110dc924edd13b803a419e71280ff0ac
HungDoan2023 <cukku2022@gmail.com>
2024-10-05 14:10:54 +0700

    Refactor code

--- setup/workspace-setup.sh
+++ setup/workspace-setup.sh
@@ -6,26 +6,26 @@ set -e
 
 project_dir=$(pwd)
 
-echo "Configuring backend application"
+echo "Configuring backend-service application"
 cd backend
-echo "Cleaning backend environment..."
+echo "Cleaning backend-service environment..."
 pipenv --rm > /dev/null 2>&1 || true
 echo "Done with cleanup!"
 
-echo "Installing backend dependencies..."
+echo "Installing backend-service dependencies..."
 pipenv install > /dev/null 2>&1
-echo "Done configuring backend!"
+echo "Done configuring backend-service!"
 cd $project_dir
 
-echo "Configuring frontend application"
+echo "Configuring frontend-service application"
 cd frontend
-echo "Cleaning frontend environment..."
+echo "Cleaning frontend-service environment..."
 npm run clean > /dev/null 2>&1
 
-echo "Installing frontend dependencies..."
+echo "Installing frontend-service dependencies..."
 nvm install > /dev/null 2>&1
 npm ci --silent > /dev/null 2>&1
-echo "Done configuring frontend!"
+echo "Done configuring frontend-service!"
 cd $project_dir
 
 echo "Done setting up workspace 🚀"
